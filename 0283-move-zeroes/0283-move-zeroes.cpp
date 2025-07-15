a0b012acd8450906956d8f31d5036beeccb21ed5
Aryan Suryavanshi <155088296+SuryavanshiRN@users.noreply.github.com>
2025-07-15 21:29:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int left = 0;
-
-        for (int right = 0; right < nums.size(); right++) {
-            if (nums[right] != 0) {
-                swap(nums[right], nums[left]);
-                left++;
+        int n = nums.size();
+        int i=0;
+        for(int j=0;j<n-1;j++){
+            if(nums[j]==0){
+                if(nums[j+1]!=0){
+                    swap(nums[i],nums[j+1]);
+                    i++;
+                }
             }
-        }        
+        }
     }
 };
