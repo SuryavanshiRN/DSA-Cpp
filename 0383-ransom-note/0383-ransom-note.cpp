a8a28cb2357f2e222537952b00fae191f7f6ec19
Aryan Suryavanshi <155088296+SuryavanshiRN@users.noreply.github.com>
2025-07-20 20:38:59 +0530

    Time: 4 ms (48.48%), Space: 11.9 MB (52.54%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp1;
+        unordered_map<char,int> mp2;
+
+        for(char c: ransomNote){
+            mp1[c]++;
+        }
+        for(char c: magazine){
+            mp2[c]++;
+        }
+        for(auto &it: mp1){
+            int var = it.first;
+            if(it.second > mp2[var]) return false;
+        }
+        return true;
+
+    }
+};
