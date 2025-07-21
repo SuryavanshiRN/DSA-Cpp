d0f36f24c9db2d48670b041ed7d73c0ea2a2dd56
Aryan Suryavanshi <155088296+SuryavanshiRN@users.noreply.github.com>
2025-07-21 21:58:01 +0530

    Time: 15 ms (72.52%), Space: 24.8 MB (85.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,16 +1,40 @@
 class Solution {
 public:
+    
+    string generate(string &s) {
+        int count[26] = {0};
+        
+        for(char &ch : s) {
+            count[ch-'a']++;
+        }
+        
+        string new_s;
+        
+        for(int i = 0; i<26; i++) {
+            
+            if(count[i] > 0) {
+                new_s += string(count[i], i+'a');
+            }
+        }
+        
+        return new_s;
+    }
+    
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-         vector<vector<string>> result;
-         unordered_map<string,vector<string>> mp;
-         for(int i=0;i<strs.size();i++){
-            string temp = strs[i];
-            sort(temp.begin(),temp.end());
-            mp[temp].push_back(strs[i]);
-         }
-         for(auto it: mp){
-            result.push_back(it.second);
-         }
-         return result;
+        unordered_map<string, vector<string>> mp;
+        
+        for(string &s : strs) {
+            string new_s = generate(s);
+            
+            mp[new_s].push_back(s);
+        }
+        
+        vector<vector<string>> result;
+        for(auto &it : mp) {
+            result.push_back(std::move(it.second));
+        }
+        
+        return result;
+        
     }
 };
