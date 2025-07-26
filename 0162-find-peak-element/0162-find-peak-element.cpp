860e2c9a6692e68e9d697c7638ef884a9d9c064f
Aryan Suryavanshi <155088296+SuryavanshiRN@users.noreply.github.com>
2025-07-26 17:48:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        return max_element(nums.begin(), nums.end()) - nums.begin();
+        int n = nums.size();
+        int l = 0;
+        int u = n - 1;
+        while(l<=u){
+            int m = l + (u - l)/2;
+            if(nums[m]<nums[m+1]) l = m + 1;
+            else u = m;
+        } 
+
+        return l;
     }
 };
