5d40fda46906b530c8e58a80227d0246c61a47b3
Aryan Suryavanshi <155088296+SuryavanshiRN@users.noreply.github.com>
2025-08-09 10:25:42 +0530

    Time: 7 ms (40.87%), Space: 15.7 MB (54.64%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,21 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
-
-        long long total = 0;
-
-        for(int i = 0; i<n;i++){
-            int mi = nums[i];
-            int mx = nums[i];
-            for(int j=i;j<n;j++){
-                mi = min(mi, nums[j]);
-                mx = max(mx, nums[j]);
-
-                total = total + (mx - mi);
-            }
+        
+        // --- Sum of maximums ---
+        vector<int> ngl(n), ngr(n);
+        stack<int> st;
+        // Nearest Greater to Left
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] < nums[i]) st.pop();
+            ngl[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        while (!st.empty()) st.pop();
+        // Nearest Greater to Right
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] <= nums[i]) st.pop();
+            ngr[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        long long sumMax = 0;
+        for (int i = 0; i < n; i++) {
+            long long left = i - ngl[i];
+            long long right = ngr[i] - i;
+            sumMax += (long long)nums[i] * left * right;
         }
 
-        return total;
+        // --- Sum of minimums ---
+        vector<int> nsl(n), nsr(n);
+        while (!st.empty()) st.pop();
+        // Nearest Smaller to Left
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] > nums[i]) st.pop();
+            nsl[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        while (!st.empty()) st.pop();
+        // Nearest Smaller to Right
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] >= nums[i]) st.pop();
+            nsr[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        long long sumMin = 0;
+        for (int i = 0; i < n; i++) {
+            long long left = i - nsl[i];
+            long long right = nsr[i] - i;
+            sumMin += (long long)nums[i] * left * right;
+        }
+
+        return sumMax - sumMin;
     }
 };
