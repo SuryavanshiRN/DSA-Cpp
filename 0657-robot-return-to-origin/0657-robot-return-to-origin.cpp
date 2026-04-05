2eefe7c0fae3813d4318d297e3b437a312759274
Aryan Suryavanshi <155088296+SuryavanshiRN@users.noreply.github.com>
2026-04-05 12:51:16 +0530

    Time: 4 ms (27.83%), Space: 10.5 MB (13.45%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+
+        for(int i=0; i<moves.size(); i++){
+            if(moves[i]=='U') y++;
+            if(moves[i]=='D') y--;
+            if(moves[i]=='L') x--;
+            if(moves[i]=='R') x++;
+        }
+
+        return (x==0 and y==0);
+    }
+};
